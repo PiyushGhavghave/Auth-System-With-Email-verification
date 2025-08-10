f188739a754608e72d650e90caed6eb913871927
PiyushGhavghave <piyushghavghave@gmail.com>
2025-08-10 22:02:05 +0530

    added utilities - setup completed

---
+++ Backend/src/server.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import connectDB from './config/connectDB.js';
+import app from './app.js'
+const port = process.env.PORT || 3000;
+
+connectDB()
+.then(() => {
+    app.listen(port, () => {
+        console.log(`Appliaction is serving on port ${port}`);
+    })
+})
+.catch((err) => {
+    console.log("Mongodb Connection failed : ", err);
+})
